4d19b7951fe5a50aeaf30a94a9bcdef12ea0ae91
k.genki0913 <k.genki0913@icloud.com>
2026-04-05 16:04:23 +0000

    spring入門追加

---
+++ src/di/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -0,0 +1,27 @@
+package com.example.demo.chapter03.aop;
+
+import java.text.SimpleDateFormat;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SampleAspect {
+    @Before("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    public void beforeAdvice(JoinPoint joinPoint) {
+        System.out.println("===== Before Advice =====");
+        System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
+        System.out.println(String.format("メソッド:%s", joinPoint.getSignature().getName()));
+    }
+
+    @After("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        System.out.println("===== After Advice =====");
+        System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
+        System.out.println(String.format("メソッド名:%s", joinPoint.getSignature().getName()));
+    }
+}
